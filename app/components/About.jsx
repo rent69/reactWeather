0b6b9f46b8d64d13e0246e59decf876efb37ca58
rent69 <rent.sidon@gmail.com>
2017-03-31 10:58:05 -0700

    Styled About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
+      <h1 className="text-center">About</h1>
+      <p>Here are some of the tools and code behind this Weather App (built for Complete React App Developer Course):</p>
+      <ol>
+        <li>
+          <a href='http://www.openweathermap.org'>Open Weather Map</a>  -- Free weather API
+        </li>
+        <li>
+          <a href='https://facebook.github.io/react'>React</a> -- Javascript framework
+        </li>
+        <li>
+          <a href='https://github.com/rent69/reactWeather'>Github</a> -- Code repository
+        </li>
+      </ol>
     </div>
   )
 };
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Santa Barbara'>Santa Barbara, CA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio De Janeiro'>Rio De Janeiro, Brazil</Link>
+        </li>
+      </ol>
     </div>
   )
 };
