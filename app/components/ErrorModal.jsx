7b3a33a464d9e0095b75ac0d6c507ce1282e754d
rent69 <rent.sidon@gmail.com>
2017-04-01 17:09:20 -0700

    fixed DOM conflict in errorModal.jsx

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
   GetDefaultProps: function () {
@@ -13,23 +15,34 @@ propTypes: {
 },
 
 componentDidMount: function () {
+    var {title, message} = this.props;
+  var modalMarkup = (
+    <div id="error-modal" className="reveal tiny text-center" data-reveal=''>
+       <h4>{title}</h4>
+       <p>{message}</p>
+       <p>
+         <button className="button hollow" data-close=''>
+           Try Again
+         </button>
+       </p>
+
+    </div>
+  );
+
+  var $modal = $(reactDomServer.renderToString(modalMarkup));
+  $(ReactDOM.findDomNode(this)).html($modal);
+
   var modal = new Foundation.Reveal($('#error-modal'));
   modal.open();
 },
 render: function () {
-  var {title, message} = this.props;
- return   (
-   <div id="error-modal" className="reveal tiny text-center" data-reveal=''>
-      <h4>{title}</h4>
-      <p>{message}</p>
-      <p>
-        <button className="button hollow" data-close=''>
-          Try Again
-        </button>
-      </p>
+ return (
+   <div>
 
    </div>
  );
+
+
 }
 
 });
